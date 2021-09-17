0fdbbe7a308c9ab87fca179ebae1211ae06a12d7
Arc <33088785+arcbtc@users.noreply.github.com>
2021-09-17 15:17:08 +0100

    Update LNURLPoS.ino

--- LNURLPoS/LNURLPoS.ino
+++ LNURLPoS/LNURLPoS.ino
@@ -4,7 +4,16 @@
 #include <Keypad.h>
 #include <string.h>
 #include "qrcode.h"
-#include <qrcode.h>
+#include "Bitcoin.h"
+#include "utility/segwit_addr.h"
+
+////////CHANGE///////
+
+String lnbits = "https://lnbits.com";
+String posId = "9j309f3f320fm042m3f";
+String posSecret = "f234gf3rg3rwg3rg3";
+
+/////////////////////
 
 String dataId = "";
 bool paid = false;
@@ -12,7 +21,7 @@ bool shouldSaveConfig = false;
 bool down = false;
 const char* spiffcontent = "";
 String spiffing; 
-
+String lnurl;
 String choice;
 String payhash;
 String key_val;
@@ -27,6 +36,7 @@ String nosats;
 float conversion;
 String virtkey;
 String payreq;
+String randomPin;
 bool settle = false;
 
 // The custom font file attached to this sketch must be included
@@ -49,8 +59,9 @@ char keys[rows][cols] = {
   {'7','8','9'},
   {'*','0','#'}
 };
-byte rowPins[rows] = {12, 14, 27, 26}; //connect to the row pinouts of the keypad
-byte colPins[cols] = {25, 33, 32}; //connect to the column pinouts of the keypad
+//21 22 17
+byte rowPins[rows] = {12, 13, 15, 2}; //connect to the row pinouts of the keypad
+byte colPins[cols] = {17, 22, 21}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 int checker = 0;
 char maxdig[20];
@@ -60,50 +71,36 @@ void setup(void) {
   pinMode (2, OUTPUT);
   digitalWrite(2, HIGH);
   tft.begin();
-  tft.setRotation(1);
+  tft.setRotation(3);
+
   logo();
   delay(3000);
 }
 
 void loop() {
   inputs = "";
-  page_input();
+  settle = false;
   displaySats(); 
   bool cntr = false;
   while (cntr != true){
    char key = keypad.getKey();
    if (key != NO_KEY){
      virtkey = String(key);
-     
+     Serial.println(virtkey);
        if (virtkey == "#"){
-        page_processing();
-
-        qrShowCode("ssdfsdf");
+        randomPin = String(random(1000,9999));
+        makeLNURL(lnbits + "/lnurlpos/api/" + posId + "/" + randomPin + "/" + inputs);
+        qrShowCode(lnurl);
         int counta = 0;
          while (settle != true){
-           counta++;
            virtkey = String(keypad.getKey());
-         
            if (virtkey == "*"){
             tft.fillScreen(TFT_BLACK);
-            tft.setCursor(52, 40);
-            tft.setTextSize(1);
-            tft.setTextColor(TFT_RED);
-            tft.println("CANCELLED");
-            delay(1000);
             settle = true;
             cntr = true;
            }
            else if (virtkey == "#"){
-            makeLNURL();
-            delay(1000);
-            settle = true;
-            cntr = true;
-           }
-           
-           if(counta >30){
-            settle = true;
-            cntr = true;
+            showPin();
            }
          }
        }
@@ -121,7 +118,31 @@ void loop() {
       displaySats();    
     }
   }
- 
+}
+
+//helpers
+
+void to_upper(char * arr){
+  for (size_t i = 0; i < strlen(arr); i++)
+  {
+    if(arr[i] >= 'a' && arr[i] <= 'z'){
+      arr[i] = arr[i] - 'a' + 'A';
+    }
+  }
+}
+
+void makeLNURL(String XXX){
+  char Buf[200];
+  XXX.toCharArray(Buf, 200);
+  char *url = Buf;
+  byte * data = (byte *)calloc(strlen(url)*2, sizeof(byte));
+  size_t len = 0;
+  int res = convert_bits(data, &len, 5, (byte *)url, strlen(url), 8, 1);
+
+  char * charLnurl = (char *)calloc(strlen(url)*2, sizeof(byte));
+  bech32_encode(charLnurl, "lnurl", data, len);
+  to_upper(charLnurl);
+  lnurl = charLnurl;
 }
 
 //display functions
@@ -147,8 +168,20 @@ void qrShowCode(String lnurl){
     }
 }
 
-void page_input()
+void showPin()
 {
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setFreeFont(SMALLFONT);
+  tft.setCursor(0, 20);
+  tft.println("PAYMENT PROOF PIN");
+  tft.setCursor(60, 80);
+  tft.setTextColor(TFT_RED, TFT_BLACK); 
+  tft.setFreeFont(BIGFONT);
+  tft.println(randomPin);
+}
+
+void displaySats(){
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);      // White characters on black background
   tft.setFreeFont(MIDFONT);
@@ -157,38 +190,15 @@ void page_input()
   tft.setCursor(60, 130);
   tft.setFreeFont(SMALLFONT);
   tft.println("TO RESET PRESS *");
-}
-
-void page_processing()
-{ 
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(49, 40);
-  tft.setTextSize(1);
-  tft.setTextColor(TFT_WHITE);
-  tft.println("PROCESSING");
- 
-}
-
-void displaySats(){
-  inputs += virtkey;
-  float temp = float(inputs.toInt()) / 100;
-  fiat = String(temp);
-  satoshis = temp/conversion;
-  int intsats = (int) round(satoshis*100000000.0);
-  Serial.println(intsats);
   
-  nosats = String(intsats);
+  inputs += virtkey;
+  float amount = float(inputs.toInt()) / 100;
   tft.setFreeFont(BIGFONT);
   tft.setTextColor(TFT_RED, TFT_BLACK);
-  tft.setCursor(50, 80);
-  tft.println(fiat);
+  tft.setCursor(10, 80);
+  tft.println(amount);
   delay(100);
   virtkey = "";
-
-}
-
-void makeLNURL(){
-
 }
 
 void logo(){
